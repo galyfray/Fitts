90b2df53f89c289b6ee3eab56b5dfadea6915044
Galyfray <galyfray@gmail.com>
2021-04-06 17:21:49 +0200

    include

--- view/mainwindow.cpp
+++ view/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "../controller/controller.h"
-#include "../model/model.h"
+#include "controller/controller.h"
+#include "model/model.h"
 
 #include <QWidget>
 #include <QHBoxLayout>
